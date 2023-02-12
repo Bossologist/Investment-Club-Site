ccf91335fddad4adb43ef29e706fddd252925679
Bossologist <85655686+Bossologist@users.noreply.github.com>
2023-02-11 18:29:06 -0800

    Update create-team.js

--- page-code/create-team.js
+++ page-code/create-team.js
@@ -15,25 +15,32 @@ function hashCode(str) {
 }
 
 $w.onReady(async function () {
+	$w('#nameInput').value = "akdfsjlsakdjf";
 });
 
 export async function createTeam(event) {
+	$w('#createTeamButton').disable();
+	$w('#createTeamButton').label = "Loading...";
+
 	//get data about user+contest
 	let id = local.getItem("id");
 	let contest = session.getItem("contest");
 
 	//get inputs
 	let name = $w("#nameInput").value;
-	let pwd = $w("#passwordInput").value;
-	let confirmPwd = $w("#confirmPassInput").value;
-	let hash = hashCode(pwd);
+	let hash;
+	if (!$w("#passwordInput").hidden) {
+		let pwd = $w("#passwordInput").value;
+		let confirmPwd = $w("#confirmPassInput").value;
+		hash = hashCode(pwd);
 
-	$w("#passwordInput").onCustomValidation( (value, reject) => {if(pwd.length < 8) reject("Password must be at least 8 characters");});
-	$w("#passwordInput").onCustomValidation( (value, reject) => {if(pwd != confirmPwd) reject("Passwords do not match");}, false);
-	$w("#confirmPassInput").onCustomValidation( (value, reject) => {if(pwd != confirmPwd) reject("Passwords do not match");}, false);
+		$w("#passwordInput").onCustomValidation( (value, reject) => {if(pwd.length < 8) reject("Password must be at least 8 characters");});
+		$w("#passwordInput").onCustomValidation( (value, reject) => {if(pwd != confirmPwd) reject("Passwords do not match");}, false);
+		$w("#confirmPassInput").onCustomValidation( (value, reject) => {if(pwd != confirmPwd) reject("Passwords do not match");}, false);
 
-	$w("#passwordInput").updateValidityIndication();
-	$w("#confirmPassInput").updateValidityIndication();
+		$w("#passwordInput").updateValidityIndication();
+		$w("#confirmPassInput").updateValidityIndication();
+	} else hash = -1;
 
 	if ($w("#passwordInput").valid) {
 		//create team
@@ -42,5 +49,27 @@ export async function createTeam(event) {
 		await addTeam(contest, name, id, data);
 
 		wixLocation.to('/contests');
+	} else {
+		$w('#passwordInput').value = "";
+		$w('#confirmPassInput').value = "";
+		$w('#passInfoText').text = "* " + $w('#passwordInput').validationMessage;
+		$w('#passInfoText').show();
+	}
+
+	$w('#createTeamButton').enable();
+	$w('#createTeamButton').label = "Create Team";
+}
+
+export function publicSwitch_change(event) {
+	if (!$w("#publicSwitch").checked) {
+		$w("#passwordInput").hide();
+		$w("#confirmPassInput").hide();
+		$w("#passwordInput").required = false;
+		$w("#confirmPassInput").required = false;
+	} else {
+		$w("#passwordInput").show();
+		$w("#confirmPassInput").show();
+		$w("#passwordInput").required = true;
+		$w("#confirmPassInput").required = true;
 	}
 }
