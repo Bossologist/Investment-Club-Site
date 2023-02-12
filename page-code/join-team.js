dcbbb33d46b6dbf3f6360887041e9a4f2f9c2988
Bossologist <85655686+Bossologist@users.noreply.github.com>
2023-02-11 18:29:46 -0800

    Update join-team.js

--- page-code/join-team.js
+++ page-code/join-team.js
@@ -17,9 +17,19 @@ $w.onReady(async function () {
         repData.push({"_id": i.toString(), "teamId": team.teamId, "pwd": team.pwd});
     }
     $w("#joinContestRep").data = repData;
+
+    $w("#joinContestRep").forEachItem( ($item, itemData, index) => {
+        console.log("index: " + index);
+        if (teamsData[index].teamSize >= 4) {
+            $item("#joinButton").label = "Full Team";
+            $item("#joinButton").disable(); 
+        }
+    })
 });
 
 export function joinButton_click(event) {
+    $w('#joinButton').disable();
+    $w('#joinButton').label = "Loading...";
 	let contest = session.getItem("contest");
 	let id = local.getItem("id");
 
@@ -33,9 +43,15 @@ export function joinButton_click(event) {
     const teamId = teamData.teamId;
     const pwd = teamData.pwd;
 
-    if (pwd != -1) wixWindow.openLightbox("Enter Password", {"pwd": pwd, "teamId": teamId, "userId": id, "contest": contest});
+    if (pwd != -1) {
+        wixWindow.openLightbox("Enter Password", {"pwd": pwd, "teamId": teamId, "userId": id, "contest": contest})
+            .then( (data) => {if (data.joined) wixLocation.to("/contests");});
+    }
     else {
         addUserToTeam(contest, id, teamId);
         wixLocation.to('/contests');
     }
+
+    $w('#joinButton').enable();
+    $w('#joinButton').label = "Join Team";
 }
